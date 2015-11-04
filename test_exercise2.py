344674240d27091782f0000bc19c6018c4b3fe16
Darius Chow <dariuschow@gmail.com>
2015-11-04 02:06:32 -0500

    test_exercise2.py: Added a bunch of test cases.

--- test_exercise2.py
+++ test_exercise2.py
@@ -19,12 +19,45 @@ def test_find_basic():
     Test find function.
     """
     assert find("This is an ex-parrot", "parrot", 0, 20) == 14
+    assert find("This is an ex-parrot", "parrot", 14, 20) == 14
+    assert find("Who lives in a pineapple under the sea", "sea", 0, 38) == 35
 
+def test_find_not_present():
+    """
+    Test find function when substring is not present in input_string, or outside index range.
+    """
+    assert find("This is an ex-parrot", "parrot", 0, 19) == -1
+    assert find("This is an ex-parrot", "parrot", 15, 20) == -1
+    assert find("Who lives in a pineapple under the sea", "sea", 0, 30) == -1
+    assert find("", "parrot", 0, 0) == -1
 
 def test_multi_find_basic():
     """
     Test multi_find function.
     """
     assert multi_find("Ni! Ni! Ni! Ni!", "Ni", 0, 15) == "0,4,8,12"
+    assert multi_find("Catdogcatdog", "cat", 0, 12) == "6"
+    assert multi_find("Catdogcatdog", "Cat", 0, 12) == "0"
+    assert multi_find("Catdogcatdog", "dog", 0, 12) == "3,9"
+    assert multi_find("aaaaa", "a", 0, 5) == "0,1,2,3,4"
+    assert multi_find("aaaaa", "aaaaa", 0, 5) == "0"
+    assert multi_find("aaaaa", "aaa", 1, 4) == "1"
 
-def test
+def test_multi_find_overlap():
+    """
+    Test multi_find function.
+    """
+    assert multi_find("lololololol", "lol", 0, 11) == "0,2,4,6,8"
+    assert multi_find("lololololol", "lol", 3, 10) == "4,6"
+    assert multi_find("aaaaa", "aa", 0, 5) == "0,1,2,3"
+    assert multi_find("aaaaa", "aaa", 0, 5) == "0,1,2"
+    assert multi_find("aaaaa", "aaa", 1, 5) == "1,2"
+
+def test_multi_find_basic_not_present():
+    """
+    Test multi_find function when substring is not present in input_string, or outside index range.
+    """
+    assert multi_find("Ni! Ni! Ni! Ni!", "ni", 0, 15) == ""
+    assert multi_find("Who lives in a pineapple under the sea", "spongebob", 0, 38) == ""
+    assert multi_find("Who lives in a pineapple under the sea", "sea", 0, 30) == ""
+    assert multi_find("", "ni", 0, 0) == ""
